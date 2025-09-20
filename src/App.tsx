028d65ce7b9919f639c3ebcc0632c225ed21ed0e
masapico <masapico8@gmail.com>
2025-09-20 10:02:10 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container, Row } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <p>hello</p>
+      </Row>
+    </Container>
+  );
+};
+
+export default App;
